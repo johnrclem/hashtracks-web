241d87dd8802dde4a7fd58f27fb921c6c933803c
johnrclem <johnrobertclem@gmail.com>
2026-02-21 22:39:52 -0500

    Fix Hangover Cheerio type for production build

--- src/adapters/html-scraper/hangover.ts
+++ src/adapters/html-scraper/hangover.ts
@@ -1,4 +1,5 @@
 import * as cheerio from "cheerio";
+import type { AnyNode } from "domhandler";
 import type { Source } from "@/generated/prisma/client";
 import type { SourceAdapter, RawEventData, ScrapeResult, ErrorDetails } from "../types";
 import { generateStructureHash } from "@/pipeline/structure-hash";
@@ -111,7 +112,7 @@ function resolveUrl(baseUrl: string, href: string | undefined): string {
   }
 }
 
-function extractIsoDateFromArticle(article: cheerio.Cheerio<cheerio.Element>): string | undefined {
+function extractIsoDateFromArticle(article: cheerio.Cheerio<AnyNode>): string | undefined {
   const datetime = article.find("time[datetime]").first().attr("datetime");
   if (!datetime) return undefined;
 
